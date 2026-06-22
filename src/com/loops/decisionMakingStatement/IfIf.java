1a92f86ba97984f962c44cc89e01adb883a761e8
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-22 22:50:06 +0530

    Added vote eligibility program using if statement

--- src/com/loops/decisionMakingStatement/IfIf.java
+++ src/com/loops/decisionMakingStatement/IfIf.java
@@ -1,5 +1,14 @@
 package com.loops.decisionMakingStatement;
 
 public class IfIf {
-
+	
+	public static void main(String [] args) {
+		int age = 22;
+		if(age>=18) {
+			System.out.println(age+ ": Person is eligible to vote");
+		}
+		if(age<= 18) {
+			System.out.println( age+ ": Person is not eligible to vote");
+		}
+	}
 }
