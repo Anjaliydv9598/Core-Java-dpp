995bd559e66ebe47964e38b34e2422acb47cceff
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-23 19:43:42 +0530

    WAJP to check the num is +ve/-ve

---
+++ src/com/loops/decisionMakingStatement/IfElse.java
@@ -0,0 +1,15 @@
+package com.loops.decisionMakingStatement;
+
+public class IfElse {
+
+	public static void main(String [] args) {
+		
+		int num=18;
+		if(num>=0) {
+			System.out.println("The given number is positive .");
+		}
+		else {
+			System.out.println("The given number is negative .");
+		}
+	}
+}
