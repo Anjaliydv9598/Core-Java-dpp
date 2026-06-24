6de5ebe826499ae83a454455166007290f7e0be3
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-06-24 23:53:00 +0530

    WAJP the given no. is % by 5

---
+++ src/com/loops/decisionMakingStatement/If.java
@@ -0,0 +1,15 @@
+package com.loops.decisionMakingStatement;
+
+public class If {
+
+	public static void main(String[] args) {
+		
+		int num=20;
+		
+		if(num%5==0) {
+			System.out.println("The given number is divisible by 5");
+		}
+
+	}
+
+}
