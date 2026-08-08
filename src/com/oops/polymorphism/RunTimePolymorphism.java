89376c6e211e1142167819661090679f71ecac72
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-08-09 01:33:36 +0530

    WAJP for Runtime Polymorphism

--- src/com/oops/polymorphism/RunTimePolymorphism.java
+++ src/com/oops/polymorphism/RunTimePolymorphism.java
@@ -1,5 +1,33 @@
 package com.oops.polymorphism;
 
+class Payment{
+	public void pay() {
+		System.out.println("Processing Payment ...");
+	}	
+}
+
+class CreditCard extends Payment{
+	@Override
+	public void pay() {
+		System.out.println("Payment done using Credit Card");
+	}	
+}
+
+class UPI extends Payment{
+	@Override
+	public void pay() {
+		System.out.println("Payment done using UPI");
+	}
+}
+
 public class RunTimePolymorphism {
+	public static void main(String[] args) {
+		Payment p;
+		p=new CreditCard();
+		p.pay();
+		
+		p=new UPI();
+		p.pay();	//Method call depends on object type, not reference type 
+	}
 
 }
