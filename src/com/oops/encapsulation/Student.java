2c222c3f818b9a1c3da86fa9050cf2e1d207f7fb
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-08-08 21:54:51 +0530

    WAJP for Encapsulation

---
+++ src/com/oops/encapsulation/Student.java
@@ -0,0 +1,38 @@
+package com.oops.encapsulation;
+
+public class Student {
+	
+	private int rollno;
+	private String name;
+	private double marks;
+	
+	public  void setRollno(int rollno) {
+		this.rollno=rollno;
+	}
+	
+	public int getRollno() {
+		return rollno;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setSalary(double marks) {
+		this.marks=marks;
+	}
+	
+	public double getMarks() {
+		return marks;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+
+}
