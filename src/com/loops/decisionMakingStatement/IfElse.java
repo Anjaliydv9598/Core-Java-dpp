bf23199d727949ccb822102859094045d6ecbab8
Anjaliydv9598 <anjaliydv9598@gmail.com>
2026-08-11 11:16:53 +0530

    Added If Else statement

--- src/com/loops/decisionMakingStatement/IfElse.java
+++ src/com/loops/decisionMakingStatement/IfElse.java
@@ -4,24 +4,32 @@ public class IfElse {
 
 	public static void main(String [] args) {
 		
-//		int num=18;
-//		
-//		if(num>=0) {
-//			System.out.println("The given number is positive .");
-//		}
-//		else {
-//			System.out.println("The given number is negative .");
-//		}
+		//the given no is +ve or not
+		int num=18;
+		
+		if(num>=0) {
+			System.out.println("The given number is positive .");
+		}
+		else {
+			System.out.println("The given number is negative .");
+		}
+		
 		
 		
 		
 		// if sal is greater than 10,000 then add 2000 if not then add 1000
 		int salary = 24000;
+		
 		if(salary>10000) {
 			salary=salary+2000;
 		}else {
 			salary=salary+1000;
 		}
 		System.out.println(salary);
+		
+		
+		
+		
+		
 	}
 }
